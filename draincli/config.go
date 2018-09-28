93e006c246b656aab71c199eb4d4f50fadda377f
Brady Love <blove@pivotal.io>
2018-09-28 13:46:01 -0600

    Change env variable for skip ssl validation on draincli tests
    [#160554932]
    
    Signed-off-by: Travis Patterson <tpatterson@pivotal.io>

--- draincli/config.go
+++ draincli/config.go
@@ -12,7 +12,7 @@ type TestConfig struct {
 	CFAdminPassword string `env:"CF_ADMIN_PASSWORD, required"`
 	CFDomain        string `env:"CF_DOMAIN,         required"`
 
-	SkipCertVerify bool `env:"SKIP_CERT_VERIFY"`
+	SkipCertVerify bool `env:"SKIP_SSL_VALIDATION"`
 
 	DefaultTimeout time.Duration `env:"DEFAULT_TIMEOUT"`
 	AppPushTimeout time.Duration `env:"APP_PUSH_TIMEOUT"`
